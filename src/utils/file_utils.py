39bd7a488b03e59e6bad1a8884cf5847b6925115
Mihail Luchian <s6miluch@uni-bonn.de>
2018-03-20 18:52:19 +0200

    ADDED : new export functions

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -1,1 +1,15 @@
 import constants as C
+import imageio
+import os.path
+
+def load_image(path):
+    return imageio.imread(path)
+
+def save_image(path,img):
+    imageio.imsave(path,img)
+
+def export_image(file_name,img):
+    imageio.imsave(os.path.join(C.PATH_FOLDER_EXPORTS,file_name),img)
+
+def load_tulip():
+    return load_image(C.PATH_FILE_TULIP)
