f1f71202f8c59ed44111d60d2fbe139948759c58
Mihail Luchian <s6miluch@uni-bonn.de>
2018-03-20 18:51:57 +0200

    ADDED : new constants and a new properties file

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,29 @@
 import os.path
 
+#
 
+R = 0
+G = 1
+B = 2
+
+LIST_CHANNELS = [R,G,B]
+
+Y = 0
+X = 1
+
+# FOLDER constants
 GO_BACK = '..'
-FOLDER_ROOT = os.path.dirname(os.path.realpath(__file__))
+PATH_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), GO_BACK)
+FOLDER_DATA = 'data'
+FOLDER_IMGS = 'imgs'
+FOLDER_EXPORT = 'exports'
+PATH_FOLDER_DATA = os.path.join(PATH_ROOT, FOLDER_DATA)
+PATH_FOLDER_IMGS = os.path.join(PATH_FOLDER_DATA,FOLDER_IMGS)
+PATH_FOLDER_EXPORTS = os.path.join(PATH_ROOT, FOLDER_EXPORT)
+
+
+# FILE
+FILE_TULIP = 'tulip.jpg'
+PATH_FILE_TULIP = os.path.join(PATH_FOLDER_IMGS,FILE_TULIP)
+
+
---
+++ src/properties.py
@@ -0,0 +1,22 @@
+# Instead of constantly writing strings everywhere, we add all possible properties for the scripts here
+
+# orientation
+X = 0
+Y = 1
+
+
+CHANNEL_RED   = 20
+CHANNEL_GREEN = 21
+CHANNEL_BLUE  = 22
+
+AVERAGE = 30
+MEDIAN = 31
+MAX = 32
+MIN = 33
+
+
+# sorting properties
+SIMPLE = 100
+RANDOM = 101
+HIGHLIGHTS = 112
+SHADOWS = 113
