4eeeb62a75d3cbf7eaff6b4c6bfeb9951d59c2ab
Mihail Luchian <mihai.luchian@gmail.com>
2019-03-04 22:39:55 +0100

    EDITED & ADDED : hierarchical markov noise, first version

--- src/scripts/noise/markov_noise.py
+++ src/scripts/noise/markov_noise.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import namedtuple
 from utils.generate_utils import *
 from utils.file_utils import *
 from utils.data_utils import  *
@@ -30,24 +31,102 @@ EXPORT_NAME = '0.png'
 OFFSET = 0.01
 
 
-markov_process_change_line_type = OFFSET + np.random.uniform(0.0, 1 - OFFSET * 2, (2,))
-markov_process_change_line_properties = OFFSET + np.random.uniform(0.0, 1 - OFFSET * 2, (2,))
-markov_process_change_line_properties = [0.05, 0.1]
-image = np.ones((HEIGHT,WIDTH))
+### There differen markov chains working in tandem,
+### one if for deciding the length, the other one is for deciding the type
 
-current_state_pixel = 0
-current_state_line = 0
-current_state_line_properties = 0
-for h in np.arange(HEIGHT):
-    current_state_line = np.random.binomial(
-        1, markov_process_change_line_type[current_state_line], (1,))[0]
-    if current_state_line == 1:
-        current_state_line_properties = np.random.binomial(
-                1, markov_process_change_line_properties[current_state_line_properties], (1,))[0]
-        if current_state_line_properties == 1:
-            ps = OFFSET + np.random.uniform(0.0, 1 - OFFSET * 2, (2,))
-        for w in np.arange(WIDTH):
-            current_state_pixel = np.random.binomial(1, ps[current_state_pixel], (1,))[0]
-            image[h,w] = current_state_pixel
 
-export_image(EXPORT_NAME,image)
+def simulate_markov_chain(transition_matrix, start_probs, length):
+
+    num_elems = len(transition_matrix)
+    r = np.arange(num_elems)
+    pseudo_simulations = np.zeros((num_elems,length)).astype('int32')
+
+    for i in range(num_elems):
+        pseudo_simulations[i] = np.random.choice(r,size=length,p=transition_matrix[i])
+
+    start_elem = np.random.choice(r,p=start_probs)
+
+    simulation = np.zeros(length)
+    simulation[0] = start_elem
+    current_elem = start_elem
+
+    for i in range(1,length):
+        new_elem = pseudo_simulations[current_elem,i]
+        simulation[i] = new_elem
+        current_elem = new_elem
+
+    return simulation.astype('int32')
+
+def simulate_markov_hierarchy(node):
+    simulation = simulate_markov_chain(
+        transition_matrix=node.transition_matrix,
+        start_probs=node.start_probs,
+        length=node.length)
+    vals = [node.values[i] for i in simulation]
+
+    if node.leaf == False:
+        for n in vals:
+            m = simulate_markov_hierarchy(n)
+            for i in m:
+                yield i
+    else:
+        yield vals
+
+
+MarkovModel = namedtuple(
+    'MarkovModel',
+    ['transition_matrix', 'start_probs','values','length','leaf'])
+
+
+px_1 = MarkovModel(
+    transition_matrix=[[0,1],[1,0]],
+    start_probs=[0.5,0.5],
+    values=['a','b'],
+    leaf=True,
+    length=10)
+
+px_2 = MarkovModel(
+    transition_matrix=[[0.1,0.9],[0.2,0.8]],
+    start_probs=[0.5,0.5],
+    values=['c','d'],
+    leaf=True,
+    length=10)
+
+px_3 = MarkovModel(
+    transition_matrix=[[0.1,0.9],[0.2,0.8]],
+    start_probs=[0.5,0.5],
+    values=['e','f'],
+    leaf=True,
+    length=10)
+
+px_4 = MarkovModel(
+    transition_matrix=[[0.1,0.9],[0.2,0.8]],
+    start_probs=[0.5,0.5],
+    values=['g','h'],
+    leaf=True,
+    length=10)
+
+node_1 = MarkovModel(
+    transition_matrix=[[0.1,0.9],[0.2,0.8]],
+    start_probs=[0.5,0.5],
+    values=[px_1,px_2],
+    leaf=False,
+    length=10)
+
+node_2 = MarkovModel(
+    transition_matrix=[[0.1,0.9],[0.2,0.8]],
+    start_probs=[0.5,0.5],
+    values=[px_3,px_4],
+    leaf=False,
+    length=10)
+
+parent = MarkovModel(
+    transition_matrix=[[0.1,0.9],[0.2,0.8]],
+    start_probs=[0.5,0.5],
+    values=[node_1,node_2],
+    leaf=False,
+    length=10)
+
+full_simulation = simulate_markov_hierarchy(parent)
+for i in full_simulation:
+    print(i)
